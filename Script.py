329740755b317ac39044dea6507d9243ca84ee82
mitalipadhiyar <mipadhiy@flemingcollege.ca>
2023-03-13 12:42:28 -0400

    Update script.py

--- Script.py
+++ Script.py
@@ -2,19 +2,10 @@ from pastebin_api import post_new_paste
 from poke_api import get_poke_name
 import sys
 
-
-
-
 def main():
     name=get_pokemon_name()
     info=get_poke_name(name)
     
-
-    if info:
-       title, body_text=poke_paste(info)
-       paste_url=post_new_paste(title,body_text,'1M')
-       print(paste_url)
-    return
 
 def get_pokemon_name():
     num_params=len(sys.argv)-1
@@ -23,14 +14,7 @@ def get_pokemon_name():
     else:
         print("Error: Missing pokemon name.")
 def poke_paste(info):
-    name=info['name'].capitalize()
-    abilities_name=[ability['ability']['name'] for ability in info['abilities']]
-    print (abilities_name)
-    abilities_list= '- ' + '\n '.join(abilities_name)
-    title= f"{name}'s Abilities"
-    body=abilities_list
-
-    return title,body 
+    return 
 
 
 
