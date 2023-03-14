2491138d94e78cfa4a5d50462a92b6888bf70dfb
mitalipadhiyar <mipadhiy@flemingcollege.ca>
2023-03-13 23:22:35 -0400

    final commit

--- poke_api.py
+++ poke_api.py
@@ -4,16 +4,15 @@ import requests
 
 
 def main():
-    url=get_poke_name('MITALI')
+    url=get_poke_name()
     pass 
 def get_poke_name(name):
 
-    poke_name=str(name).strip().lower()
-    PokeMon_url=f'https://pokeapi.co/api/v2/pokemon/{poke_name}'
+    PokeMon_url=f'https://pokeapi.co/api/v2/pokemon/{name}'
 
     resp_msg=requests.get(PokeMon_url)
 
-    print(f"Getting information for {poke_name}...",end='')
+    print(f"Getting information for {name}...",end='')
 
     if resp_msg.ok:
         print('success')
@@ -21,11 +20,7 @@ def get_poke_name(name):
     else:
         print("failure")
         print(f"Response code: {resp_msg.status_code} ({resp_msg.reason})")
-
-
     pass
-
-
 if __name__ == '__main__':
     main()
 
